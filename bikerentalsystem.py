b9fdf5ab5ca915ca5080b1781bc96ab27cd8dde7
vinayak1809 <vinayakrshinde18@gmail.com>
2021-11-08 23:59:46 +0530

    first commit

---
+++ bikerentalsystem.py
@@ -0,0 +1,158 @@
+
+class main:
+    print()
+    available_bikes = {
+        "kawasaki": 8, "ninja": 3, "unicorn": 5, "pulsur": 7
+    }
+
+    print("available bikes ::")
+    for a, b in (available_bikes.items()):
+        print(a + ": " + str(b))
+
+    rupee = u'\u20B9'
+    cost = {"hour": rupee+"400", "day": rupee+"1500", "weak": rupee+"4500"}
+    print(" ")
+
+    print("cost of bikes :: \n")
+    for a, b in (cost.items()):
+        print(a + ": " + str(b))
+
+    print(" ")
+
+    done = False
+
+    def bymore():
+        print()
+        ask = input("do u want to rent more bikes(Y/N) : ").upper()
+        if ask == "Y":
+            main.done = False
+        else:
+            exit()
+
+
+class hour_day_week:
+
+    rupee = u'\u20B9'
+
+    def hourly_basis():
+
+        for ask in main.available_bikes.keys():
+
+            if ask_bike == ask:
+                hour_price = 500
+
+                payment_tobe_paid = hour_price * num_of_bikes_want
+                if num_of_bikes_want > 3:
+                    payment_tobe_paid = payment_tobe_paid * (30/100)
+
+                print("payment to be paid is :: " +
+                      hour_day_week.rupee+str(+payment_tobe_paid))
+                payment = int(input("payment :: "))
+
+                if payment == payment_tobe_paid:
+
+                    main.available_bikes[ask] = main.available_bikes[ask] - \
+                        num_of_bikes_want
+
+                    print("Thankyou! Have a great day ")
+                    main.bymore()
+
+    def day_basis():
+        for ask in main.available_bikes.keys():
+            if ask_bike == ask:
+                day_price = 1500
+
+                payment_tobe_paid = day_price * num_of_bikes_want
+                if num_of_bikes_want > 3:
+                    payment_tobe_paid = payment_tobe_paid * (30/100)
+
+                print("payment to be paid is :: " +
+                      hour_day_week.rupee+str(payment_tobe_paid))
+                payment = int(input("payment :: "))
+
+                if payment == payment_tobe_paid:
+                    main.available_bikes[ask] = main.available_bikes[ask] - \
+                        num_of_bikes_want
+
+                    print("Thankyou! Have a great day ")
+                    main.bymore()
+
+    def weekly_basis():
+        for ask in main.available_bikes.keys():
+            if ask_bike == ask:
+                week_price = 4500
+
+                payment_tobe_paid = week_price * num_of_bikes_want
+                if num_of_bikes_want > 3:
+                    payment_tobe_paid = payment_tobe_paid * (30/100)
+
+                print("payment to be paid is :: " +
+                      hour_day_week.rupee+str(payment_tobe_paid))
+                payment = int(input("payment :: "))
+
+                if payment == payment_tobe_paid:
+                    main.available_bikes[ask] = main.available_bikes[ask] - \
+                        num_of_bikes_want
+
+                    main.bymore()
+
+
+class hapen:
+
+    def happenagain(time_limit):
+        obj = hour_day_week
+
+        if time_limit == "hour":
+            obj.hourly_basis()
+
+        elif time_limit == "day":
+            obj.day_basis()
+
+        elif time_limit == "week":
+            obj.weekly_basis()
+
+        else:
+            print("this bike is not available")
+
+
+while not main.done:
+    done_ask = False
+    return_rent = input(
+        "do u want to return or rent the bike (return/rent) :: ").lower()
+
+    if return_rent == "rent":
+        while not done_ask:
+            ask_bike = input("which bike do u want :: ").lower()
+
+            if ask_bike in main.available_bikes.keys():
+                num_of_bikes_want = int(
+                    input("how many bikes do you want :: "))
+
+                if num_of_bikes_want > main.available_bikes[ask_bike]:
+                    print("this much bikes are not available ")
+                else:
+                    time_limit = input("time you need :: ")
+                    hapen.happenagain(time_limit)
+
+            else:
+                print("retype the bike name :")
+                done_ask = False
+
+    elif return_rent == "return":
+        bike_return = input("bike name to be returned :: ").lower()
+        num_of_bikes_to_be_return = int(
+            input("num of bikes to be returned :: "))
+
+        for bikes in main.available_bikes.keys():
+            if bike_return == bikes:
+                main.available_bikes[bikes] = main.available_bikes[bikes] + \
+                    num_of_bikes_to_be_return
+                print("available bikes ::")
+                for a, b in (main.available_bikes.items()):
+                    print(a + ": " + str(b))
+
+        print("thankyou :) ")
+        exit()
+    else:
+        print("type again return/rent")
+        main.done = False
